8d9624aaa3fd62e84940ba46d31cbf24ed77b250
doddi-vignesh_infosys <doddi.vignesh@infosys.com>
2025-06-15 18:39:43 +0530

    change

--- backend/src/models/db_connection.js
+++ backend/src/models/db_connection.js
@@ -8,6 +8,10 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT || 5432,
+    ssl: {
+        require: true,
+        rejectUnauthorized: false // Temporarily disable SSL verification
+    }
 });
 
 // Test the database connection
